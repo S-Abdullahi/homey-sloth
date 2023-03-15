239c8e567abc28767521512bf092ce53448b44c1
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-03-15 14:44:10 +0100

    home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-black text-gray-300 text-xs text-center p-4">
+      <p>
+        <span className="text-stone-500">HomeySloth</span> All right reserved.
+        Developed by <span className="text-stone-500">S.Abdullahi</span>
+      </p>
+      <p></p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Item } from "../components";
+import { Item, Footer } from "../components";
 import { GiCompass, GiDiamondHard, GiScrollUnfurled } from "react-icons/gi";
 
 const data = [1, 2, 3];
@@ -90,13 +90,29 @@ const Home = () => {
       {/* Join Our Newsletter */}
       <section className="mx-40 my-20 flex">
         <div className="w-1/2">
-            <h3 className="text-3xl font-bold mb-4">Join our newsletter and get 20% off</h3>
-            <p className="leading-8 text-stone-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sint unde quaerat ratione soluta veniam provident adipisci cumque eveniet tempore?</p>
+          <h3 className="text-3xl font-bold mb-4">
+            Join our newsletter and get 20% off
+          </h3>
+          <p className="leading-8 text-stone-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            sint unde quaerat ratione soluta veniam provident adipisci cumque
+            eveniet tempore?
+          </p>
         </div>
-        <div className="w-1/2">
-            <input type='text' placeholder="Enter Email" className="border-2 border-black w-full"/>
-            <button className="btn">Subscribe</button>
+        <div className="w-1/2 relative mt-10">
+          <input
+            type="text"
+            placeholder="Enter Email"
+            className="border-2 border-black w-full absolute p-2 text-stone-400 rounded"
+          />
+          <button className="absolute right-0 bg-stone-400 border-2 border-black px-4 py-2 rounded">
+            Subscribe
+          </button>
         </div>
+      </section>
+      {/* footer */}
+      <section>
+        <Footer/>
       </section>
     </div>
   );
