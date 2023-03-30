81d6195058796ad69082926cab9f5478c695b406
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-03-30 22:16:13 +0100

    hover

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,7 +5,7 @@ import { useProductContext } from '../context/Products_Context'
 const Item = ({id, name, price, image}) => {
   return (
     <div className='mb-2 '>
-        <img src={image} alt='product' className='mb-2 rounded h-40 w-full object-cover'/>
+        <img src={image} alt='product' className='mb-2 rounded h-40 w-full object-cover cursor-pointer hover:contrast-50'/>
         <div className='flex justify-between text-sm'>
             <p>{name}</p>
             <p className='text-stone-500'>$ {price}</p>
